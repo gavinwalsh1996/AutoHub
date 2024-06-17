ef6423164322d90d857a16bcf94e3bc1caf67bc3
Gavin Walsh <gavinwalshwork@gmail.com>
2024-06-17 10:49:14 -0700

    Added quick fix to tailwind styles not applying

--- autohub/src/app/components/Navbar.tsx
+++ autohub/src/app/components/Navbar.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+
+import CustomButton from "./CustomButton"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className="w-full absolute z-10">
+        <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-purple-500">
+            <Link href="/" className="flex justofy-center items-center">
+                <Image src="/logo.svg" alt="AutoHub Logo" width={118}
+                height={18} className="object-contain"></Image>
+            </Link>   
+        </nav>
+    </header>
   )
 }
 

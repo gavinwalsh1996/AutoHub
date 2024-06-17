37637836f3306ea260a895951a91aa0c39399113
Gavin Walsh <gavinwalshwork@gmail.com>
2024-06-17 10:30:04 -0700

    Changes added and updating dependencies to fix tailwind processing error

--- autohub/src/app/components/Hero.tsx
+++ autohub/src/app/components/Hero.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import CustomButton from "./CustomButton"
+import Image from 'next/image'
 
 function Hero() {
     const handleScroll = () => {
-
+        console.log('test');
     }
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
             <h1 className="hero__title">
-                Find, book or rentt a car -- quickly and easily
+                Find, book or rent a car -- quickly and easily
             </h1>
 
             <p className="hero__subtitle">
@@ -22,6 +23,12 @@ function Hero() {
                 containerStyles="bg-primary-blue text-white rounded-full mt-10"
                 handleClick={handleScroll}
             />
+        </div>
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src="/hero.png" alt="hero" fill className="object-contain" />
+                <div className="hero__image-overlay"></div>
+            </div>
         </div>
     </div>
   )
